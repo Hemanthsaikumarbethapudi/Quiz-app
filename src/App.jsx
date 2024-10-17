60302827f85ba60c8701a395f2f1fedcd7080b62
hemanth <bhemanthsaikumar04@gmail.com>
2024-10-17 13:04:34 +0530

    code added

---
+++ src/App.jsx
@@ -0,0 +1,64 @@
+// import logo from './logo.svg';
+// import './App.css';
+import "./style.css";
+function App() {
+  return (
+    <div className="App">
+      <div className="main">
+        <h1>main</h1>
+      </div>
+      <div className="pyramid">
+        <ul className="moneylist">
+          <li className="moneyListnum">
+            <span className="moneylistnum">10</span>
+            <span className="moneylistamo">$5000</span>
+          </li>
+          <li className="moneyListnum">
+            <span className="moneylistnum">9</span>
+            <span className="moneylistamo">$4000</span>
+          </li>
+          <li className="moneyListnum">
+            <span className="moneylistnum">8</span>
+            <span className="moneylistamo">$3000</span>
+          </li>
+          <li className="moneyListnum">
+            <span className="moneylistnum">7</span>
+            <span className="moneylistamo">$2000</span>
+          </li>
+          <li className="moneyListnum">
+            <span className="moneylistnum">6</span>
+            <span className="moneylistamo">$1000</span>
+          </li>
+          <li className="moneyListnum">
+            <span className="moneylistnum">5</span>
+            <span className="moneylistamo">$500</span>
+          </li>
+          <li className="moneyListnum">
+            <span className="moneylistnum">4</span>
+            <span className="moneylistamo">$400</span>
+          </li>
+          <li className="moneyListnum">
+            <span className="moneylistnum">3</span>
+            <span className="moneylistamo">$300</span>
+          </li>
+          <li className="moneyListnum">
+            <span className="moneylistnum">2</span>
+            <span className="moneylistamo">$200</span>
+          </li>
+          <li className="moneyListnum">
+            <span className="moneylistnum">1</span>
+            <span className="moneylistamo">$100</span>
+          </li>
+
+
+
+
+
+
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
